ce601c3db52a2ea7857faf1e7e7b7494c2df61fa
rezabuddawla <rezab@sharetrip.net>
2024-07-10 18:19:30 +0600

    adding selec2 library and configuration

--- config/params.php
+++ config/params.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'bsDependencyEnabled' => false,
+    'bsVersion' => '5.x',
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
